949ca2ab94f5fc9d442974e3baaa446549b5ff06
groon <groon112@mail.ru>
2022-10-07 00:10:47 +0300

    init

---
+++ Package/jut_su_parse.py
@@ -0,0 +1,219 @@
+import re
+from time import time
+import progressbar
+import requests
+import tqdm
+from bs4 import BeautifulSoup
+from datetime import datetime
+# from data.test import write_json
+import json
+
+links = {}
+down_links = []
+headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                         "Chrome/99.0.4844.84 Safari/537.36 OPR/85.0.4341.79"}
+
+
+def write_json(file_name, value):
+    with open(f'{file_name}', 'w', encoding='utf-8') as f:
+        json.dump(value, fp=f, ensure_ascii=False, indent=4)
+
+
+def check_time(*a_args, **k_kwargs):
+    def name_func(func):
+        def wrapper(*args, **kwargs):
+            start = datetime.now()
+            result = func(*args, **kwargs)
+            print(f'Фнукция "{" ".join(a_args)}" отработала за: {datetime.now() - start}')
+            return result
+
+        return wrapper
+
+    return name_func
+
+
+def check_anime_global(link):
+    anime_global = False
+    links.clear()
+    req = requests.get(link, headers=headers)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    all_anime_global = soup.find_all('div', class_='all_anime_global')
+
+    if not all_anime_global:
+        name = soup.find('h1', class_='header_video allanimevideo anime_padding_for_title').text \
+            .replace('Смотреть ', '').replace(' все серии и сезоны', '').replace(' все серии', '')
+    else:
+        name = soup.find('h1', class_='mail_h').text.replace('Все серии аниме ', '')
+        anime_global = True
+
+    links.update({"link": link, "last_series": "", "select_series": "", "select_season": "", "name": name})
+    return soup, anime_global
+
+
+def get_episodes(soup, anime_global):
+    """
+    # Аниме_глобал - это Наруто. Мб найду что-то ещё.
+    # По умолчанию False. В данном случае на одной странице все ссылки на серии.
+    # Парсим именно их
+    """
+    temp_dict = {}
+    if not anime_global:
+        try:
+            episodes = soup.findAll('a', class_="short-btn")
+            # Список сезонов на русском
+            season_titles = [i.text for i in soup.find_all('h2', class_="the-anime-season")]
+
+        # Если сезонов нет или что-то пошло не так - возвращаем ничего
+        except requests.exceptions.MissingSchema:
+            return None
+
+        # Парсим эпизоды, они же кнопки серии.
+        for i in episodes:
+            # Собирает все серии со страницы
+            episode_title = re.search(r'\d+\s+\w+$', i.text).group()
+            link_episode = i['href']
+            if "https://jut.su" not in link_episode:
+                link_episode = "https://jut.su" + link_episode
+
+            if re.search(r'season+-\d+', link_episode):
+                season = re.search(r'season+-\d+', link_episode).group()
+                # Находим номер сезона для серии
+                season_title = int(re.search(r'\d+', season).group())
+                if season_titles[season_title - 1] not in links.keys():
+                    # Минус 1, т.к. s_title - это номер сезона. s_titles же - список сезонов.
+                    # Чтобы найти нужный - вычитаем 1, т.к. отсчёт идёт с нуля.
+                    # links.update({season: {season: season_titles[season_title - 1], "episode_list": []}})
+                    links.update({season_titles[season_title - 1]: {"name": season_titles[season_title - 1], 'series': {}}})
+                # links[season]['episode_list'].append({"link": link_episode, "name": episode_title})
+                links[season_titles[season_title-1]]['series'].update({episode_title: link_episode})
+
+            elif re.search(r'film-\d+', link_episode):
+                if season_titles[-1] not in links.keys():
+                    links.update({season_titles[-1]: {"name": season_titles[-1], 'series': {}}})
+                links[season_titles[-1]]['series'].update({episode_title: link_episode})
+            else:
+                if "Все серии" not in links.keys():
+                    links.update({'Все серии': {"name": "Все серии", "series": {}}})
+                links['Все серии']['series'].update({episode_title: link_episode})
+
+    else:
+        seasons = soup.find_all('div', class_='all_anime_global')
+        links_anime_global = []
+
+        for season_global in seasons:
+            season_title = season_global.find('div', class_='aaname').text \
+                .replace(season_global.find('span', class_='the_invis').text, '')
+            link = season_global.find('a', href=True)['href']
+            if "https://jut.su" not in link:
+                link = "https://jut.su" + link
+            links.update({season_title: {"name": season_title, "series": {}}})
+            req = requests.get(link, headers=headers)
+            soup = BeautifulSoup(req.content, 'html.parser')
+            episodes = soup.findAll('a', class_="short-btn")
+            if not episodes:
+                ova_data = soup.find('div', class_='watch_list_item').find_all('a', href=True)
+                for i in ova_data:
+                    # Собирает все серии со страницы
+                    link_episode = i['href']
+                    if "https://jut.su" not in link_episode:
+                        link_episode = "https://jut.su" + link_episode
+                    links[season_title]['series'].update({i.text: link_episode})
+
+            for i in episodes:
+                # Собирает все серии со страницы
+                episode_title = re.search(r'\d+\s+\w+$', i.text).group()
+                link_episode = i['href']
+                if "https://jut.su" not in link_episode:
+                    link_episode = "https://jut.su" + link_episode
+                links[season_title]['series'].update({episode_title: link_episode})
+        # write_json('series_list.json', links)
+        return links
+
+    if not links:
+        return None
+    else:
+        return links
+
+
+def get_link(episode_list):  # DOPILIT
+    if episode_list.isdecimal():
+        link = links[int(episode_list) - 1]
+        return get_download_link(link)
+    else:
+        list_links = re.split(r'\D', episode_list)
+        list_links = [links[i] for i in range(int(list_links[0]) - 1, int(list_links[-1]))]
+        for i in list_links:
+            get_download_link(i)
+        return down_links
+
+
+def get_download_link(link):
+    req1 = requests.get(link, headers=headers)
+    soup = BeautifulSoup(req1.content, 'html.parser')
+    try:
+        if soup.find("source", attrs={"label": "1080p"}).get('src'):
+            download_link = soup.find("source", attrs={"label": "1080p"}).get('src')
+        elif soup.find("source", attrs={"label": "720p"}).get('src'):
+            download_link = soup.find("source", attrs={"label": "720p"}).get('src')
+        elif soup.find("source", attrs={"label": "480p"}).get('src'):
+            download_link = soup.find("source", attrs={"label": "480p"}).get('src')
+        elif soup.find("source", attrs={"label": "360p"}).get('src'):
+            download_link = soup.find("source", attrs={"label": "360p"}).get('src')
+        else:
+            exit()
+            return
+    except AttributeError:
+        return "Данное видео недоступно для РФ или отсутствуюет"
+
+    return download_link
+
+
+def dwn(video):
+    if isinstance(video, list):
+        for link in tqdm.tqdm(video):
+            d_video = requests.get(link, stream=True, headers=headers)
+            file_size = int(d_video.headers['Content-Length'])
+            bar = progressbar.ProgressBar(maxval=file_size).start()
+            i = 0
+            name = str(re.search(r'\d+\.(1080|720|480|360)', str(link)).group())  # Допилить регулярки
+            f = open(f'onepiece-{name}.mp4', 'wb')
+            for chunk in d_video.iter_content(chunk_size=8192):
+                f.write(chunk)
+                # print(str(i) + '/' + str(file_size), end="\r")
+                bar.update(i)
+                i += 8192
+            f.close()
+    # elif isinstance(video, str):
+    #     d_video = requests.get(video, stream=True, headers=headers)
+    #     file_size = int(d_video.headers['Content-Length'])
+    #     chunk = 1
+    #     nub_bars = file_size / chunk
+    #     bar = progressbar.ProgressBar(maxval=nub_bars).start()
+    #     i = 0
+    #     f = open('one.mp4', 'wb')
+    #     for chunk in d_video.iter_content(chunk_size=8192):
+    #         f.write(chunk)
+    #         # print(str(i) + '/' + str(file_size), end="\r")
+    #         bar.update(i)
+    #         i += 8192
+    #     f.close()
+
+
+@check_time('main', 'jut_su_parse.py')
+def main(start_link):
+    if re.fullmatch(r'https://jut\.su/.+/', start_link):
+        soup, a_global = check_anime_global(start_link)
+        if not get_episodes(soup, a_global):
+            return None
+    elif re.fullmatch(r'https://jut\.su/.+/(episode|film).+', start_link):
+        soup, a_global = check_anime_global(re.search(r'https://jut\.su/[^/]+/', start_link).group())
+        if not get_episodes(soup, a_global):
+            return None
+    else:
+        return None
+
+    return links
+
+
+if __name__ == '__main__':
+    main()
---
+++ data/test.py
@@ -0,0 +1,339 @@
+from kivy.animation import Animation
+from kivy.properties import ColorProperty
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.lang import Builder
+from kivy.app import App
+from kivymd.app import MDApp
+from kivy.uix.popup import Popup
+import json
+
+from kivymd.uix.menu import MDDropdownMenu
+
+from Package import jut_su_parse as j_parse
+from math import ceil
+from kivymd.uix.snackbar import Snackbar
+from kivymd.theming import ThemeManager
+from kivy.metrics import dp
+
+# Window.maximize()
+Builder.load_file("../kv/start_window.kv")
+BUTTON_HEIGHT = 50
+series_dict = {}
+anime_dict = {}
+
+
+def write_json(file_name: str, value: dict):
+    with open(f'{file_name}', 'w', encoding='utf-8') as f:
+        json.dump(value, fp=f, ensure_ascii=False, indent=4)
+
+
+def load_json(file_name: str):
+    with open(f'{file_name}', encoding='utf-8') as f:
+        value = json.load(f)
+    return value
+
+
+def bad_link():
+    Snackbar(text="Вставьте корректную ссылку!",
+             snackbar_x="20dp",
+             snackbar_y="20dp",
+             size_hint_x=(Window.width - 40) / Window.width).open()
+
+
+class Main(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    @j_parse.check_time("main", "on enter")
+    def on_enter(self):
+        global anime_dict
+        anime_dict = load_json('anime_list.json')
+        b_height = len(anime_dict['film_list']) * BUTTON_HEIGHT
+        self.ids.favourites_list.height = b_height
+        for i in anime_dict['film_list']:
+            b_height -= BUTTON_HEIGHT
+            self.ids.favourites_list.add_widget(
+                Button(text=str(i['name']), on_press=self.pressing, size_hint=(1, None),
+                       height=BUTTON_HEIGHT - 2, pos=(0, b_height)))
+
+    def pressing(self, instance):
+        global series_dict
+        # anime_list = load_json('anime_list.json')
+        for i in anime_dict['film_list']:  # anime_list['film_list']:
+            if i['name'] == instance.text:
+                data = j_parse.main(i['link'])
+                if data:
+                    # write_json('series_list.json', a)  # Попробую загнать всё в переменную, а не файл
+                    series_dict = data
+                    # write_json('new.json', series_dict)
+                    self.manager.transition.direction = 'left'
+                    self.manager.current = 'second'
+                else:
+                    bad_link()
+
+    @j_parse.check_time("check for link")
+    def find_by_link(self):
+        global series_dict
+        a = j_parse.main(self.ids.input_name.text)
+        if a:
+            # write_json('series_list.json', a)
+            series_dict = a
+            self.manager.current = 'second'
+        else:
+            bad_link()
+
+
+class Second(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.series_count = []
+        menu_items = [
+            {
+                "viewclass": "OneLineListItem",
+                "text": "Скачать всё",
+                "height": dp(56),
+                "on_release": lambda x="Скачать всё": self.menu_callback(x)
+            },
+            {
+                "viewclass": "OneLineListItem",
+                "text": "Настройки",
+                "height": dp(56),
+                "on_release": lambda x="Настройки": self.menu_callback(x)
+            }
+        ]
+        self.menu = MDDropdownMenu(
+            items=menu_items,
+            width_mult=4,
+        )
+
+    def on_enter(self):
+        self.ids.ani.title = series_dict["name"]
+        series_count = list(series_dict.keys())[5:]
+        b_height = len(series_count) * BUTTON_HEIGHT
+        self.ids.season_list.height = b_height
+        for i in series_count:
+            name = series_dict[i]['name']
+            b_height -= BUTTON_HEIGHT
+            self.ids.season_list.add_widget(
+                Button(text=name, on_press=self.pressing, size_hint=(1, None),
+                       height=BUTTON_HEIGHT - 2, pos=(0, b_height)))
+
+        temp_dict = {"name": series_dict['name'],
+                     "link": series_dict['link'],
+                     "last_series": series_dict['last_series']}
+        if temp_dict in anime_dict['film_list']:
+            self.ids.ani.right_action_items = [["star-off-outline", lambda x: self.add_favourite()],
+                                               ["dots-vertical", lambda x: self.callback(x)]]
+        else:
+            self.ids.ani.right_action_items = [["star-outline", lambda x: self.add_favourite()],
+                                               ["dots-vertical", lambda x: self.callback(x)]]
+
+    def pressing(self, instance):
+        global series_dict
+        if self.manager.current != 'Second':
+            series_dict["select_season"] = instance.text
+            # write_json('series_list.json', self.series_list)
+            # series_dict.update(self.series_list)
+            self.manager.transition.direction = 'left'
+            if len(series_dict[instance.text]['series'].keys()) > 50:
+                self.manager.current = 'fourth'
+            else:
+                self.manager.current = self.manager.next()
+
+    def on_leave(self):
+        # self.ids.anime_name.text = ''
+        self.ids.season_list.clear_widgets()
+        self.ids.ani.title = ''
+
+    def set_previous_screen(self):
+        if self.manager.current != 'Main':
+            self.manager.transition.direction = 'right'
+            self.manager.current = self.manager.previous()
+
+    def add_favourite(self):
+        # self.anime_list = anime_dict  # load_json('anime_list.json')
+        # self.series_list = series_dict  # load_json('series_list.json')
+        temp_dict = {"name": series_dict['name'],
+                     "link": series_dict['link'],
+                     "last_series": series_dict['last_series']}
+
+        if self.ids.ani.right_action_items[0][0] == "star-outline":
+            anime_dict['film_list'].append(temp_dict)
+            self.ids.ani.right_action_items = [["star-off-outline", lambda x: self.add_favourite()],
+                                               ["dots-vertical", lambda x: x]]
+
+        else:
+            anime_dict['film_list'].remove(temp_dict)
+            self.ids.ani.right_action_items = [["star-outline", lambda x: self.add_favourite()],
+                                               ["dots-vertical", lambda x: x]]
+        write_json('anime_list.json', anime_dict)
+        # anime_dict.update(self.anime_list)
+
+    def callback(self, button):
+        self.menu.caller = button
+        self.menu.open()
+
+    def menu_callback(self, text_item):
+        self.menu.dismiss()
+        print(text_item)
+
+
+class Third(Screen):
+    overlay_color = ColorProperty("#5b5b5b")
+    default_color = ColorProperty("#808080")
+    progress_round_color = "#bcbcbc"
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.season = {}
+        self.series_name = ""
+        self.selection_series = []
+        menu_items = [
+            {
+                "viewclass": "OneLineListItem",
+                "text": "Скачать всё",
+                "height": dp(56),
+                "on_release": lambda x="Скачать всё": self.select_all(x)
+            },
+            {
+                "viewclass": "OneLineListItem",
+                "text": "Выбрать серии",
+                "height": dp(56),
+                "on_release": lambda x="Выбрать серии": self.menu_callback(x)
+            }
+        ]
+        self.menu = MDDropdownMenu(
+            items=menu_items,
+            width_mult=4,
+        )
+
+    @j_parse.check_time('third enter')
+    def on_enter(self, *args):
+        select_season = None
+        # self.series_list = series_dict  # load_json('series_list.json')
+        # self.ids.season_name.text = series_dict['select_season']
+        self.ids.ani.title = series_dict['select_season']
+        series_list = list(series_dict.keys())[5:]
+        for season in series_list:
+            if series_dict['select_season'] == series_dict[season]['name']:
+                select_season = series_dict[season]['series']
+
+        b_height = ceil((len(select_season)/4)) * BUTTON_HEIGHT
+        self.ids.series_list.height = b_height + 5
+        for series in select_season:
+            if len(series) > 29 and self.ids.series_list.cols != 1:
+                self.ids.series_list.cols = 1
+            b_height -= BUTTON_HEIGHT
+            # self.ids.series_list.add_widget(Button(text=series, on_press=self.pressing, size_hint=(1, None),
+            #                                        height=BUTTON_HEIGHT, pos=(0, b_height)))
+
+            self.ids.series_list.add_widget(Button(text=series, on_release=self.pressing, size_hint=(1, None),
+                                                   height=BUTTON_HEIGHT))
+
+    def set_previous_screen(self):
+        if self.manager.current != 'second':
+            self.manager.transition.direction = 'right'
+            self.manager.current = self.manager.previous()
+
+    def pressing(self, instance):
+        global series_dict
+        if self.manager.current != 'Second':
+            series_dict["select_series"] = instance.text
+            self.manager.transition.direction = 'left'
+            # self.manager.current = self.manager.next()
+
+    def on_leave(self):
+        self.ids.series_list.clear_widgets()
+        self.ids.ani.title = ''
+
+    def download(self, link_list):
+        s = [i.instance_item.text for i in self.selection_series]
+        print(s)
+
+    def select_all(self, item):
+        self.ids.series_list.selected_all()
+        self.menu.dismiss()
+
+    def callback(self, button):
+        self.menu.caller = button
+        self.menu.open()
+
+    def menu_callback(self, text_item):
+        self.menu.dismiss()
+        print(text_item)
+
+    def set_selection_mode(self, instance_selection_list, mode):
+        if mode:
+            md_bg_color = self.overlay_color
+            left_action_items = [
+                [
+                    "close",
+                    lambda x: self.ids.series_list.unselected_all(),
+                ]
+            ]
+            right_action_items = [["download", lambda x: self.download(x)], ["dots-vertical"]]
+        else:
+            md_bg_color = self.default_color
+            print(md_bg_color)
+            left_action_items = [["arrow-left", lambda x: self.set_previous_screen()]]
+            right_action_items = [["magnify", lambda x: self.download(x)],
+                                  ["dots-vertical", lambda x: self.callback(x)]]
+            self.ids.ani.title = series_dict['select_season']
+
+        Animation(md_bg_color=md_bg_color, d=.5).start(self.ids.ani)
+        self.ids.ani.left_action_items = left_action_items
+        self.ids.ani.right_action_items = right_action_items
+
+    def on_selected(self, instance_selection_list, instance_selection_item):
+        self.ids.ani.title = str(
+            len(instance_selection_list.get_selected_list_items())
+        )
+        self.selection_series = instance_selection_list.get_selected_list_items()
+        print(instance_selection_item.instance_item.text)
+
+    def on_unselected(self, instance_selection_list, instance_selection_item):
+        if instance_selection_list.get_selected_list_items():
+            self.ids.ani.title = str(
+                len(instance_selection_list.get_selected_list_items())
+            )
+
+
+class Fourth(Screen):  # Для примера. Нужно переделать
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.list_buttons = [
+            '7', '8', '9',
+            '4', '5', '6',
+            '1', '2', '3',
+            'None', '0', 'None',
+        ]
+        for i in self.list_buttons:
+            self.ids.this_layout.add_widget(Button(text=i, on_press=self.pressing))
+
+    def pressing(self, instance):
+        self.manager.current = 'first'
+
+
+class TestApp(MDApp):
+
+    def __init__(self, **kwargs):
+        self.theme_cls = ThemeManager()
+        self.title = "Jut.su"
+        self.icon = "../Sources/favicon.ico"
+        super().__init__(**kwargs)
+
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        sm = ScreenManager()
+        sm.add_widget(Main(name='first'))
+        sm.add_widget(Second(name='second'))
+        sm.add_widget(Third(name='third'))
+        sm.add_widget(Fourth(name='fourth'))
+        return sm
+
+
+if __name__ == "__main__":
+    TestApp().run()
